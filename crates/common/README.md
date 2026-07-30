bf70c2874c5a905abaa00d14fdbc127185f586ad
Henry Baldwin <henrymbaldwin@proton.me>
2026-07-30 16:50:29 -0400

    Revert "chore: initial commit and scaffold project"
    This reverts commit 15d113cd15e45338061f8f8b9078b867e292000a.

--- crates/common/README.md
+++
@@ -1,3 +0,0 @@
-# Common
-
-A small crate containing some common helper code.
--- crates/example/src/lib.rs
+++ crates/example/src/lib.rs
@@ -1,3 +1,6 @@
+//! Example crate.
+
+/// Adds two u64s together
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
